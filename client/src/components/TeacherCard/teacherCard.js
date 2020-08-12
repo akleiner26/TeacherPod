2a7c2362765f35137f34e6086727e36ff6f4cef5
Gary Bergman <garybergman00@gmail.com>
2020-08-12 12:30:02 -0400

    teacherCard formatting

--- client/src/components/TeacherCard/teacherCard.js
+++ client/src/components/TeacherCard/teacherCard.js
@@ -13,23 +13,23 @@ const Example = (props) => {
                 <Card className="cardText cardContainer">
                     <Row>
                         <Col xs="2">
-                            <CardImg className="cardThumb" top width="100%" src={props.thumbnail} alt={props.name} />
+                            <CardImg className="cardThumb cardInfo" top width="100%" src={props.thumbnail} alt={props.name} />
                         </Col>
                         {/* <CardBody> */}
                         <Col xs="2">
-                            <CardTitle ><strong>{props.name}</strong></CardTitle>
+                            <CardTitle className="cardInfo" ><strong>{props.name}</strong></CardTitle>
                         </Col>
                         <Col xs="2">
-                            <CardSubtitle >{props.subject}</CardSubtitle>
+                            <CardSubtitle className="cardInfo">{props.subject}</CardSubtitle>
                         </Col>
                         <Col xs="2">
-                            <CardText >Price: {props.price}</CardText>
+                            <CardText className="cardInfo">Price: {props.price}</CardText>
                         </Col>
                         <Col xs="2">
-                            <CardText >Pod Size: {props.capacity}</CardText>
+                            <CardText className="cardInfo">Pod Size: {props.capacity}</CardText>
                         </Col>
                         <Col xs="2">
-                            <Button className="cardBtn"><i class="fa fa-comments" aria-hidden="true"></i></Button>
+                            <Button className="cardBtn cardInfo"><i class="fa fa-comments" aria-hidden="true"></i></Button>
                         </Col>
                         {/* </CardBody> */}
                     </Row>

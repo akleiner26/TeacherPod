eef9c5f86183eab263f64a9a3cd640cbe99268fe
Esther Min <estherjmin@gmail.com>
2020-08-11 21:38:34 -0400

    added routes to create users, teachers and pods

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const studentRoutes = require("./students");
+const podRoutes = require("./pods");
 const messageRoutes = require("./messages");
 
 // User routes
@@ -8,6 +9,9 @@ router.use("/users", userRoutes);
 
 // Student routes
 router.use("/students", studentRoutes);
+
+// Pod routes
+router.use("/pods", podRoutes);
 
 // Message routes
 router.use("/messages", messageRoutes);

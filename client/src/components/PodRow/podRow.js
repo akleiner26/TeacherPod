9db68d6f34bbe0719d2b96976314e423b8e904aa
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-13 15:51:59 -0400

    table added to profile page

---
+++ client/src/components/PodRow/podRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from "./podRow.css"
+
+
+const PodRow = (props) => {
+    return (
+
+        <tr className="vertAlign">
+            <td className="vertAlign">{props.name}</td>
+            <td className="vertAlign">{props.grade}</td>
+            <td className="vertAlign">{props.slots}</td>
+            <td className="vertAlign">{props.price}</td>
+            <td className="vertAlign">{props.location}</td>
+            <td className="vertAlign">{props.availability}</td>
+            <td className="vertAlign"><i class="fa fa-book fa-2x" aria-hidden="true"></i></td>
+        </tr>
+    );
+};
+
+export default PodRow;

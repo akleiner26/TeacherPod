5f0b8349234ce9fb698178ff735f9b5ec8ebc529
Gary Bergman <garybergman00@gmail.com>
2020-08-14 14:35:23 -0400

    Styled Login and Signup pages

--- client/src/components/Signup/signup.js
+++ client/src/components/Signup/signup.js
@@ -5,45 +5,49 @@ import style from "./signup.css"
 function Signup() {
     return (
         <>
-            <Container>
-                <Card className="signupCard">
-                    <CardTitle className="text-center signupTitle">Signup
+            <Row className="mt-5 profileCardRow">
+                <Col xs="8" className="offset-2">
+                    <Container>
+                        <Card className="signupCard">
+                            <CardTitle className="text-center loginTitle darkGrayText">SIGNUP
                     <hr className="signupLine"></hr>
-                    </CardTitle>
-                    <CardBody>
+                            </CardTitle>
+                            <CardBody>
 
-                        <Form>
-                            <Row form>
-                                <Col md={6}>
+                                <Form>
+                                    <Row form>
+                                        <Col md={6}>
+                                            <FormGroup>
+                                                <Label for="firstName">First Name</Label>
+                                                <Input type="text" name="firstName" id="exampleFirstName" placeholder="First Name" />
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={6}>
+                                            <FormGroup>
+                                                <Label for="lastName">Last Name</Label>
+                                                <Input type="text" name="lastName" id="exampleLastName" placeholder="Last Name" />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
                                     <FormGroup>
-                                        <Label for="firstName">First Name</Label>
-                                        <Input type="text" name="firstName" id="exampleFirstName" placeholder="First Name" />
+                                        <Label for="exampleEmail">Email</Label>
+                                        <Input type="email" name="email" id="exampleEmail" placeholder="test@example.com" />
                                     </FormGroup>
-                                </Col>
-                                <Col md={6}>
                                     <FormGroup>
-                                        <Label for="lastName">Last Name</Label>
-                                        <Input type="text" name="lastName" id="exampleLastName" placeholder="Last Name" />
+                                        <Label for="examplePassword">Password</Label>
+                                        <Input type="password" name="password" id="examplePassword" placeholder="Enter password here" />
                                     </FormGroup>
-                                </Col>
-                            </Row>
-                            <FormGroup>
-                                <Label for="exampleEmail">Email</Label>
-                                <Input type="email" name="email" id="exampleEmail" placeholder="test@example.com" />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="examplePassword">Password</Label>
-                                <Input type="password" name="password" id="examplePassword" placeholder="Enter password here" />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="confirmPassword">Confirm Password</Label>
-                                <Input type="password" name="confirmpassword" id="confirmPassword" placeholder="Confirm password here" />
-                            </FormGroup>
-                            <Button className="btnHover hvr-fade">Submit</Button> Already a member? <a className="hrefColor" href="/login">Login </a>
-                        </Form>
-                    </CardBody>
-                </Card>
-            </Container>
+                                    <FormGroup>
+                                        <Label for="confirmPassword">Confirm Password</Label>
+                                        <Input type="password" name="confirmpassword" id="confirmPassword" placeholder="Confirm password here" />
+                                    </FormGroup>
+                                    <Button className="btnHover hvr-fade">Submit</Button> Already a member? <a className="iconHvr-fade2" href="/login">Login </a>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Container>
+                </Col>
+            </Row>
         </>
     )
 }

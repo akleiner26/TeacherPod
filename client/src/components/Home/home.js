491d0438f65b99d27f7de0945c96c10562cfe73d
Esther Min <estherjmin@gmail.com>
2020-08-14 15:15:45 -0400

    setup search form to pull data from axios get route and edited seeds for db

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -5,6 +5,7 @@ import Header from "../Header/header";
 import Footer from "../Footer/footer";
 import TeacherTable from "../TeacherTable/teacherTable";
 import Search from "../Search/search";
+import API from "../../utils/API"
 import { Container, Table } from "reactstrap";
 import style from "./home.css";
 
@@ -12,13 +13,44 @@ import style from "./home.css";
 function Home() {
     const [loggedIn, setLogin] = useState("");
     const [username, setUsername] = useState("");
+    const [search, setSearch] = useState({
+        location: "",
+        grades: "",
+        price: ""
+    })
 
     console.log(loggedIn, username);
 
+    // Captures input from search
+    const handleInputChange = event => {
+        console.log(event.target.value)
+
+        setSearch({
+            ...search,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // Kicks off search with set criteria when "submit" is clicked
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(search);
+
+        API.getTeachers(search)
+            .then(res => {
+                console.log("API call is working...")
+                console.log(res)
+            })
+
+    }
+
     return (
         <>
-            <Header loggedIn={loggedIn} username={username} func={{setLogin, setUsername}} />
-            <Search />
+            <Header loggedIn={loggedIn} username={username} func={{ setLogin, setUsername }} />
+            <Search
+                handleInputChange={handleInputChange}
+                handleFormSubmit={handleFormSubmit}
+            />
             <TeacherTable />
             <Footer />
         </>
--- seeders/seeds.js
+++ seeders/seeds.js
@@ -80,7 +80,7 @@ db.Student.deleteMany({})
         let podSeed = [
             {
                 name: "3rd Grade Math",
-                grade: "3",
+                grade: "3rd Grade",
                 slots: "6",
                 price: "250",
                 location: "remote",
@@ -88,7 +88,7 @@ db.Student.deleteMany({})
             },
             {
                 name: "3rd Grade History",
-                grade: "3",
+                grade: "3rd Grade",
                 slots: "10",
                 price: "220",
                 location: "remote",
@@ -96,7 +96,7 @@ db.Student.deleteMany({})
             },
             {
                 name: "5 Stars Pod",
-                grade: "5",
+                grade: "5th Grade",
                 slots: "5",
                 price: "175",
                 location: "remote",
@@ -104,119 +104,119 @@ db.Student.deleteMany({})
             },
             {
                 name: "5 Stars Pod Premium",
-                grade: "5",
+                grade: "5th Grade",
                 slots: "5",
                 price: "375",
                 location: "remote"
             },
             {
                 name: "Lucky 7",
-                grade: "7",
+                grade: "7th Grade",
                 slots: "8",
                 price: "245",
                 location: "19128"
             },
             {
                 name: "10th Grade",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "300",
                 location: "19123"
             },
             {
                 name: "General 10th",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "250",
                 location: "19123"
             },
             {
                 name: "Honors 10th",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "375",
                 location: "19123"
             },
             {
                 name: "5th Curriculum",
-                grade: "5",
+                grade: "5th Grade",
                 slots: "4",
                 price: "150",
                 location: "remote"
             },
             {
                 name: "5th Curriculum II",
-                grade: "5",
+                grade: "5th Grade",
                 slots: "4",
                 price: "150",
                 location: "remote"
             },
             {
                 name: "Grand Five",
-                grade: "5",
+                grade: "5th Grade",
                 slots: "8",
                 price: "200",
                 location: "remote"
             },
             {
                 name: "AP 10th",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "425",
                 location: "19128"
             },
             {
                 name: "Honors 10th",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "375",
                 location: "19125"
             },
             {
                 name: "Super 5",
-                grade: "5",
+                grade: "5th Grade",
                 slots: "10",
                 price: "250",
                 location: "19125"
             },
             {
                 name: "First Grade!",
-                grade: "1",
+                grade: "1st Grade",
                 slots: "6",
                 price: "300",
                 location: "19125"
             },
             {
                 name: "Fast 4",
-                grade: "4",
+                grade: "4th Grade",
                 slots: "8",
                 price: "225",
                 location: "remote"
             },
             {
                 name: "7th Honors",
-                grade: "7",
+                grade: "7th Grade",
                 slots: "8",
                 price: "245",
                 location: "remote"
             },
             {
                 name: "The 1st Milestone",
-                grade: "1",
+                grade: "1st Grade",
                 slots: "10",
                 price: "175",
                 location: "remote"
             },
             {
                 name: "AP 10th",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "475",
                 location: "remote"
             },
             {
                 name: "General 10th",
-                grade: "10",
+                grade: "10th Grade",
                 slots: "10",
                 price: "355",
                 location: "remote"
@@ -244,7 +244,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/lillianWoodsImg.jpg",
-                                gradesTaught: "third",
+                                gradesTaught: "3rd Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[0]._id), mongoose.Types.ObjectId(podData.ops[1]._id)],
                                 bio: "Hello! I'm Ms. Woods and I have been teaching the third grade in Chandler district since 2012. My bachelor's and master's degrees are from Penn State University. I am all about teaching from the heart and providing students with the skills and tools that they will need to develop into bright, young, successful adults!"
                             },
@@ -255,7 +255,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/angelaDavidsonImg.jpg",
-                                gradesTaught: "fifth",
+                                gradesTaught: "5th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[2]._id), mongoose.Types.ObjectId(podData.ops[3]._id)],
                                 bio: "Hi there, I'm Ms. Davidson from Wishockon. I began working at Wishockon Elementary in 2000 as a substitute teacher and moved on to teach full-time in 2006. I believe that every child is gifted in their own way and my goal is help identify those gifts so that we can support and develop them."
                             },
@@ -267,7 +267,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19128",
                                 image: "images/fullSize/tedKellyImg.jpg",
-                                gradesTaught: "seventh",
+                                gradesTaught: "7th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[4]._id)],
                                 bio: "Ahoy there! The name's Ted Kelly. I'm all about creating an encouraging and engaging environment that fosters creativity and confidence! My talent is bringing out each student's unique character and personality so that they can learn at a pace that is comfortable for them."
                             },
@@ -279,7 +279,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19123",
                                 image: "images/fullSize/margoBurnsImg.jpg",
-                                gradesTaught: "tenth",
+                                gradesTaught: "10th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[5]._id)],
                                 bio: "Welcome! I believe in a learning environment in which students feel comfortable, but I'm also clearcut on setting expectations and rules. I give my best in ensuring each student's growth, and I expect the same in return from my students and will not accept anything less. Success does not come without motivation and effort. It's all about grit!"
                             },
@@ -291,7 +291,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19123",
                                 image: "images/fullSize/rogerHorneImg.jpg",
-                                gradesTaught: "tenth",
+                                gradesTaught: "10th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[6]._id), mongoose.Types.ObjectId(podData.ops[7]._id)],
                                 bio: "Hi, I'm Mr. Horne and my passion and belief has always to invest in our children because they are the future. I'm all about creating an environment that fosters diversity and growth, and I design my curriculum to fit each student's needs. This means that each student's experience may be different from another's, but rest assured that I'll still cover all their needs. It's just that I may mix things up for a change of pace if I feel that something's not working out for a student. So if you are all for the unexpected but also want your child to have a solid learning foundation, I'd love to hear from you!"
                             },
@@ -303,7 +303,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "remote",
                                 image: "images/fullSize/billyMyersImg.jpg",
-                                gradesTaught: "fifth",
+                                gradesTaught: "5th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[8]._id), mongoose.Types.ObjectId(podData.ops[9]._id)],
                                 bio: "Hello, Mr. Myers here! I come from a family of teachers so I've always felt that teaching was my calling. My job is to motivate each student and to give them an education that is both solid in foundation but also fun and interesting. My pods are smaller because I really invest in each student's growth and development. If you are looking for a personalized learning experience for your child, contact me today!"
                             },
@@ -315,7 +315,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "remote",
                                 image: "images/fullSize/erickCollierImg.jpg",
-                                gradesTaught: "fifth",
+                                gradesTaught: "5th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[10]._id)],
                                 bio: "Hi there, I'm Mrs. Collier from Wishockon. I began working at Wishockon Elementary in 2000 as a substitute teacher and moved on to teach full-time in 2006. I believe that every child is gifted in their own way and my goal is help identify those gifts so that we can support and develop them."
                             },
@@ -327,7 +327,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19128",
                                 image: "images/fullSize/lenaWoodsImg.jpg",
-                                gradesTaught: "tenth",
+                                gradesTaught: "10th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[11]._id)],
                                 bio: "Hi, I'm Miss Woods and my passion and belief has always been to invest in our children because they are the future. I'm all about creating an environment that fosters diversity and growth, and I design my curriculum to fit each student's needs. This means that each student's experience may be different from another's, but rest assured that I'll still cover all their needs. It's just that I may mix things up for a change of pace if I feel that something's not working out for a student. So if you are all for the unexpected but also want your child to have a solid learning foundation, I'd love to hear from you!"
                             },
@@ -339,7 +339,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19125",
                                 image: "images/fullSize/brigitteJordanImg.jpg",
-                                gradesTaught: "tenth",
+                                gradesTaught: "10th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[12]._id)],
                                 bio: "Hi, I'm Miss Jordan and my passion and belief has always been to invest in our children because they are the future. I'm all about creating an environment that fosters diversity and growth, and I design my curriculum to fit each student's needs. This means that each student's experience may be different from another's, but rest assured that I'll still cover all their needs. It's just that I may mix things up for a change of pace if I feel that something's not working out for a student. So if you are all for the unexpected but also want your child to have a solid learning foundation, I'd love to hear from you!"
                             },
@@ -351,7 +351,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19125",
                                 image: "images/fullSize/kenBestImg.jpg",
-                                gradesTaught: "fifth",
+                                gradesTaught: "5th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[13]._id)],
                                 bio: "Hello, Mr. Best here! I come from a family of teachers so I've always felt that teaching was my calling. My job is to motivate each student and to give them an education that is both solid in foundation but also fun and interesting. My pods are smaller because I really invest in each student's growth and development. If you are looking for a personalized learning experience for your child, contact me today!"
                             },
@@ -363,7 +363,7 @@ db.Student.deleteMany({})
                                 isTeacher: true,
                                 location: "19125",
                                 image: "images/fullSize/isabelBrowningImg.jpg",
-                                gradesTaught: "first",
+                                gradesTaught: "1st Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[14]._id)],
                                 bio: "Welcome! I am Miss Browning and I've taught the first grade since 2001. First grade is usually a huge adjustment for children as it really kicks off their educational journey. I understand each day can be filled with many challenges and responsibilities, and I believe that as a teacher, my job is to motive and guide them in the right direction."
                             },
@@ -374,7 +374,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/pollyShafferImg.jpg",
-                                gradesTaught: "fourth",
+                                gradesTaught: "4th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[15]._id)],
                                 bio: "Hi there, I'm Mrs. Shaffer from Wishockon. I began working at Wishockon Elementary in 2000 as a substitute teacher and moved on to teach full-time in 2006. I believe that every child is gifted in their own way and my goal is help identify those gifts so that we can support and develop them."
                             },
@@ -385,7 +385,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/joannMcmahonImg.jpg",
-                                gradesTaught: "seventh",
+                                gradesTaught: "7th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[16]._id)],
                                 bio: "Hi there! I'm Joann McMahon. I'm all about creating an encouraging and engaging environment that fosters creativity and confidence! My talent is bringing out each student's unique character and personality so that they can learn at a pace that is comfortable for them."
                             },
@@ -396,7 +396,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/bonitaWilcoxImg.jpg",
-                                gradesTaught: "first",
+                                gradesTaught: "1st Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[17]._id)],
                                 bio: "Welcome! I am Miss Wilcox and I've taught the first grade since 2001. First grade is usually a huge adjustment for children as it really kicks off their educational journey. I understand each day can be filled with many challenges and responsibilities, and I believe that as a teacher, my job is to motive and guide them in the right direction."
                             },
@@ -407,7 +407,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/marvaFranklinImg.jpg",
-                                gradesTaught: "tenth",
+                                gradesTaught: "10th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[18]._id)],
                                 bio: "Hi there, I'm Mrs. Franklin from Wishockon. I began working at Wishockon High in 2000 as a substitute teacher and moved on to teach full-time in 2006. I believe that every child is gifted in their own way and my goal is help identify those gifts so that we can support and develop them."
                             },
@@ -418,7 +418,7 @@ db.Student.deleteMany({})
                                 password: bcrypt.hashSync("password"),
                                 isTeacher: true,
                                 image: "images/fullSize/sophiaCalderonImg.jpg",
-                                gradesTaught: "tenth",
+                                gradesTaught: "10th Grade",
                                 pods: [mongoose.Types.ObjectId(podData.ops[19]._id)],
                                 bio: "Hi there, I'm Mrs. Calderon from Wishockon. I began working at Wishockon High in 2000 as a substitute teacher and moved on to teach full-time in 2006. I believe that every child is gifted in their own way and my goal is help identify those gifts so that we can support and develop them."
                             }

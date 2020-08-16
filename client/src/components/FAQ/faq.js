1319daee00656a96002d6de514993c3fc0e6063b
Gary Bergman <garybergman00@gmail.com>
2020-08-16 15:16:22 -0400

    Completed basics for FAQ page

--- client/src/components/FAQ/faq.js
+++ client/src/components/FAQ/faq.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container,UncontrolledCollapse, Collapse, Card, CardTitle, CardBody, Table, Form, FormGroup, Label, Input, Button, FormText, Row, CardText, Col } from "reactstrap";
+import { Container, UncontrolledCollapse, Card, CardTitle, CardBody, Button, Row } from "reactstrap";
 import "./faq.css";
 import Header from "../Header/header"
 import Footer from "../Footer/footer"
@@ -19,12 +19,22 @@ function FAQ() {
             <Row className="mt-5 profileCardRow loginSignupRowSize">
                 <Container>
                     <Card className="loginCard">
-                        <CardTitle className="text-center loginTitle darkGrayText">CONTACT
+                        <CardTitle className="text-center loginTitle darkGrayText">FAQ
                     <hr className="line"></hr>
                         </CardTitle>
-
-                        {/* <Col xs="10" className="offset-1 mt-4"> */}
                         <CardBody className="text-center toggleCardOuter">
+                            <div>
+                                <Button className="toggleButton" id="toggler" style={{ marginBottom: '1rem' }}>
+                                    What is TeachPod?
+                                </Button>
+                                <UncontrolledCollapse toggler="#toggler">
+                                    <Card className="toggleCard">
+                                        <CardBody className="toggleText">
+                                            TeachPod is a web-based application designed to help connect students and parents with teachers. With the global complexity of the Covid-19 pandemic, we know that the struggles of education has affected nearly every community. With parents unsure of school openings, we created TeachPod to help facilitate classroom style instruction while kids are unable to attend traditional education programs. Our hope is to ease the transition to at-home learning for primary and high-school education. 
+                                        </CardBody>
+                                    </Card>
+                                </UncontrolledCollapse>
+                            </div>
 
                             <div>
                                 <Button className="toggleButton" id="toggler1" style={{ marginBottom: '1rem' }}>
@@ -33,9 +43,7 @@ function FAQ() {
                                 <UncontrolledCollapse toggler="#toggler1">
                                     <Card className="toggleCard">
                                         <CardBody className="toggleText">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
-                                            similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
-                                            dignissimos esse fuga! Minus, alias.
+                                            TeachPod was designed to easily and intuitively match users with teachers in their area, price-range, and education requirements. Simply create an account, filter instructors based on your needs, and find teachers to connect with. When you view a teacher's profile, you can find out more about that teacher and what programs they're currently offering. 
                                         </CardBody>
                                     </Card>
                                 </UncontrolledCollapse>
@@ -46,11 +54,9 @@ function FAQ() {
                                     How can I request a pod?
                                 </Button>
                                 <UncontrolledCollapse toggler="#toggler2">
-                                <Card className="toggleCard">
+                                    <Card className="toggleCard">
                                         <CardBody className="toggleText">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
-                                            similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
-                                            dignissimos esse fuga! Minus, alias.
+                                            To request a pod, simply message the teacher running the pod and let them know you're interested. The teacher will then be able to discuss all the details necessary and add you to their pod. 
                                         </CardBody>
                                     </Card>
                                 </UncontrolledCollapse>
@@ -61,11 +67,9 @@ function FAQ() {
                                     How do I pay for a pod?
                                 </Button>
                                 <UncontrolledCollapse toggler="#toggler3">
-                                <Card className="toggleCard">
+                                    <Card className="toggleCard">
                                         <CardBody className="toggleText">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
-                                            similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
-                                            dignissimos esse fuga! Minus, alias.
+                                            Currently, payments will be made via electronic payment directly to the teacher. Once you join a pod, the instructor will send out a message with payment instructions. We are working on adding a PayPal payment system directly to the site. Be on the look out for that soon!
                                         </CardBody>
                                     </Card>
                                 </UncontrolledCollapse>
@@ -76,36 +80,14 @@ function FAQ() {
                                     Where do classes take place?
                                 </Button>
                                 <UncontrolledCollapse toggler="#toggler4">
-                                <Card className="toggleCard">
+                                    <Card className="toggleCard">
                                         <CardBody className="toggleText">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
-                                            similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
-                                            dignissimos esse fuga! Minus, alias.
+                                            As of now, all classes will be conducted via <a href="https://zoom.us/">Zoom</a>. Simply setup a computer with a webcam, connect with a teacher, and start learning!
                                         </CardBody>
                                     </Card>
                                 </UncontrolledCollapse>
                             </div>
-
-                            <div>
-                                <Button className="toggleButton" id="toggler5" style={{ marginBottom: '1rem' }}>
-                                    Another question?
-                                </Button>
-                                <UncontrolledCollapse toggler="#toggler5">
-                                <Card className="toggleCard">
-                                        <CardBody className="toggleText">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
-                                            similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
-                                            dignissimos esse fuga! Minus, alias.
-                                        </CardBody>
-                                    </Card>
-                                </UncontrolledCollapse>
-                            </div>
-
-                            
-
-
                         </CardBody>
-                        {/* </Col> */}
                     </Card>
                 </Container>
             </Row>

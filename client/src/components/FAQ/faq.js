45d3a9815fd48e98f18eb24ac001d6be8ca2c0ca
Gary Bergman <garybergman00@gmail.com>
2020-08-16 14:09:01 -0400

    Created toggles for faq

--- client/src/components/FAQ/faq.js
+++ client/src/components/FAQ/faq.js
@@ -1,52 +1,113 @@
 import React, { useState } from "react";
-import { Container, Card, CardTitle, CardBody, Form, FormGroup, Label, Input, Button, FormText, Row, CardText, Col } from "reactstrap";
+import { Container, Collapse, Card, CardTitle, CardBody, Table, Form, FormGroup, Label, Input, Button, FormText, Row, CardText, Col } from "reactstrap";
 import "./faq.css";
 import Header from "../Header/header"
 import Footer from "../Footer/footer"
 
 
 function FAQ() {
+    // For Header functionality
     const [loggedIn, setLogin] = useState("");
     const [username, setUsername] = useState("");
     const [id, setId] = useState("");
 
+    // For Toggle functionality
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle1 = (event) => {
+        event.preventDefault();
+        setIsOpen(!isOpen);
+    }
+
     return (
         <>
             <Header loggedIn={loggedIn} id={id} username={username} func={{ setLogin, setUsername, setId }} />
-            {/* <Row className="mt-5 profileCardRow loginSignupRowSize">
-                <Col xs="8" className="offset-2">
-                    <Container className>
-                        <Card className="loginCard">
-                            <CardTitle className="text-center loginTitle darkGrayText">LOGIN
+
+            <Row className="mt-5 profileCardRow loginSignupRowSize">
+                <Container>
+                    <Card className="loginCard">
+                        <CardTitle className="text-center loginTitle darkGreyText">CONTACT
                     <hr className="line"></hr>
-                            </CardTitle>
-                            <CardBody>
+                        </CardTitle>
 
-                                <Form>
-                                    <FormGroup>
-                                        <Label for="exampleEmail">Email</Label>
-                                        <Input type="email" name="username" id="exampleEmail" placeholder="test@example.com" onChange={handleInputChange} />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label for="examplePassword">Password</Label>
-                                        <Input type="password" name="password" id="examplePassword" placeholder="Enter password here" onChange={handleInputChange} />
-                                    </FormGroup>
-                                  
-                                        <Button className="btnHover hvr-fade" onClick={handleFormSubmit}>Submit</Button>
-                                        <div className="floatRt">
-                                            Don't have an account yet?<a className="iconHvr-fade2" href="/signup"> Signup</a>
-                                        </div>
+                        {/* <Col xs="10" className="offset-1 mt-4"> */}
+                        <CardBody className="text-center">
 
+                            <div>
+                                <Button color="primary" onClick={toggle1} style={{ marginBottom: '1rem' }}>Toggle</Button>
+                                <Collapse isOpen={isOpen}>
+                                    <Card className="toggleCard">
+                                        <CardBody className="toggleText">
+                                            Anim pariatur cliche reprehenderit,
+                                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                            nesciunt sapiente ea proident.
+                                        </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
 
-                         
+                            <div>
+                                <Button color="primary" onClick={toggle1} style={{ marginBottom: '1rem' }}>Toggle</Button>
+                                <Collapse isOpen={isOpen}>
+                                    <Card className="toggleCard">
+                                        <CardBody className="toggleText">
+                                            Anim pariatur cliche reprehenderit,
+                                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                            nesciunt sapiente ea proident.
+                                        </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
 
+                            <div>
+                                <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+                                <Collapse isOpen={isOpen}>
+                                    <Card className="toggleCard">
+                                        <CardBody className="toggleText">
+                                            Anim pariatur cliche reprehenderit,
+                                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                            nesciunt sapiente ea proident.
+                                        </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
 
-                                </Form>
-                            </CardBody>
-                        </Card>
-                    </Container>
-                </Col>
-            </Row> */}
+                            <div>
+                                <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+                                <Collapse isOpen={isOpen}>
+                                    <Card className="toggleCard">
+                                        <CardBody className="toggleText">
+                                            Anim pariatur cliche reprehenderit,
+                                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                            nesciunt sapiente ea proident.
+                                        </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
+
+                            <div>
+                                <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+                                <Collapse isOpen={isOpen}>
+                                    <Card className="toggleCard">
+                                        <CardBody className="toggleText">
+                                            Anim pariatur cliche reprehenderit,
+                                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                            nesciunt sapiente ea proident.
+                                        </CardBody>
+                                    </Card>
+                                </Collapse>
+                            </div>
+
+                        </CardBody>
+                        {/* </Col> */}
+                    </Card>
+                </Container>
+            </Row>
+
             <Footer />
         </>
     )
